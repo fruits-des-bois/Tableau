5556f068a314a6e6ad1a6cf27831f0079f158853
fruits-des-bois <noe.nguyen@etu.unilasalle.fr>
2026-09-11 13:58:33 +0200

    Update app_sup.py

--- app_sup.py
+++ app_sup.py
@@ -217,7 +217,7 @@ st.markdown(
    ============================================================ */
 
 /* Fond bleu de la zone contenant les onglets */
-.st.tabs.a {
+.st.tabs {
     background-color: #0B2742 !important;
     border-radius: 0 0 18px 18px !important;
     padding: 0 12px 10px 12px !important;
@@ -818,7 +818,7 @@ st.markdown(
 # ONGLETS
 # ============================================================
 
-tab_debit, tab_hauteur, tab_carte, tab_meteo, tab_previsions = st.tabs.a([
+tab_debit, tab_hauteur, tab_carte, tab_meteo, tab_previsions = st.tabs([
     "💧 Débit d'eau",
     "📏 Hauteur d'eau",
     "🗺️ Carte",
