7208068d3f65245de63186df32c62b8a510d562e
fruits-des-bois <noe.nguyen@etu.unilasalle.fr>
2026-09-11 13:49:07 +0200

    Update app_sup.py

--- app_sup.py
+++ app_sup.py
@@ -217,7 +217,7 @@ st.markdown(
    ============================================================ */
 
 /* Fond bleu de la zone contenant les onglets */
-.Hort {
+.header {
     background-color: #0B2742 !important;
     border-radius: 0 0 18px 18px !important;
     padding: 0 12px 10px 12px !important;
@@ -793,7 +793,6 @@ def fetch_data():
 # BANDEAU PRINCIPAL : TITRE + ONGLETS
 # ============================================================
 """
-<div class="Hort">
 with st.container(border=True):
 
    st.markdown(
@@ -815,7 +814,6 @@ with st.container(border=True):
     """,
     unsafe_allow_html=True
 )
-</div class="Hort">
 """
 
 # ===================================
