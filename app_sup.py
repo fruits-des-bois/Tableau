a60a33b22e80ceedcaaabefd34de6d45cd223f72
fruits-des-bois <noe.nguyen@etu.unilasalle.fr>
2026-09-11 13:36:08 +0200

    Update app_sup.py

--- app_sup.py
+++ app_sup.py
@@ -795,27 +795,34 @@ def fetch_data():
 
 with st.container(border=True):
 
-    st.markdown(
-        """
+   st.markdown(
+    """
+    <div style="
+        background-color: #0B2742;
+        padding: 22px 30px 16px 30px;
+        border-radius: 18px 18px 0 0;
+        margin-bottom: 0;
+    ">
         <div style="
-            background-color: #0B2742;
-            padding: 20px 25px 15px 25px;
-            margin: -1px -1px 0 -1px;
-            border-radius: 15px 15px 0 0;
+            color: white;
+            font-size: 30px;
+            font-weight: 700;
         ">
-            <div style="
-                color: white;
-                font-size: 30px;
-                font-weight: 700;
-                margin-bottom: 5px;
-            ">
-                🌊 Dashboard hydrologique de Beauvais
-            </div>
-
+            🌊 Dashboard hydrologique de Beauvais
         </div>
-        """,
-        unsafe_allow_html=True
-    )
+
+        <div style="
+            color: #C9D8E3;
+            font-size: 14px;
+            margin-top: 6px;
+        ">
+            Suivi de la hauteur d'eau, du débit,
+            des précipitations et des prévisions
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # ===================================
 # Onlets de navigation
