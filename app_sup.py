ce9a0f1a713bfe40a8c115019c697884d1adad68
fruits-des-bois <noe.nguyen@etu.unilasalle.fr>
2026-09-11 13:25:30 +0200

    Update app_sup.py

--- app_sup.py
+++ app_sup.py
@@ -44,7 +44,7 @@ st.markdown(
     }
 
 # ============================================================
-# HEADER DU DASHBOARD
+# HEADER DU DASHBOARD 1.1
 # ============================================================
 
     .dashboard-header {
@@ -58,7 +58,6 @@ st.markdown(
     
     .dashboard-title {
         color: #FFFFFF !important;
-        background: #0B2742 !important;
         font-size: 30px !important;
         font-weight: 700 !important;
         margin: 0 !important;
@@ -67,7 +66,6 @@ st.markdown(
     
     .dashboard-subtitle {
         color: #C9D8E3 !important;
-        background: #0B2742 !important;
         font-size: 14px !important;
         margin-top: 6px !important;
         margin-bottom: 0 !important;
@@ -268,9 +266,9 @@ def appliquer_theme_plotly(fig):
 
     return fig
 
-# ============================================================
+# ==============
 # HEADER
-# ============================================================
+# ==============
 
 st.markdown(
     """
