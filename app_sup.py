2880e0f6377bf7c056d16dbee22b63f84df49525
fruits-des-bois <noe.nguyen@etu.unilasalle.fr>
2026-09-11 15:22:27 +0200

    Update app_sup.py

--- app_sup.py
+++ app_sup.py
@@ -34,7 +34,7 @@ st.markdown(
        1. FOND GLOBAL ET TYPOGRAPHIE
        ============================== */
     .stApp {
-        background-color: #F2F2F2 !important; /* Bleu nuit profond */
+        background-color: #FFFFFF !important; /* Bleu nuit profond */
         color: #000000 !important;
     }
 
