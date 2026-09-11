19695325ff57ba18cbb5cad984a8a6777c244991
fruits-des-bois <noe.nguyen@etu.unilasalle.fr>
2026-09-11 11:58:02 +0200

    Update app_sup.py

--- app_sup.py
+++ app_sup.py
@@ -252,24 +252,6 @@ def appliquer_theme_plotly(fig):
 
     return fig
 
-# ==============
-# Titre
-# ==============
-
-st.markdown("""
-<div class="dashboard-header">
-
-    <div class="dashboard-title">
-        🌊 Dashboard hydrologique de Beauvais
-    </div>
-
-    <div class="dashboard-subtitle">
-        Suivi de la hauteur d'eau, du débit, des précipitations et des prévisions
-    </div>
-
-</div>
-""", unsafe_allow_html=True)
-
 # ------------------------
 # Début du code Streamlit
 # ------------------------
