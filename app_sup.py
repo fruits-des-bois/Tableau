aeaff38b4bb1af3719ce2e57618341d12bff8118
fruits-des-bois <noe.nguyen@etu.unilasalle.fr>
2026-09-11 14:12:24 +0200

    Update app_sup.py

--- app_sup.py
+++ app_sup.py
@@ -760,10 +760,6 @@ st.markdown("""
     <div class="dashboard-title">
         🌊 Dashboard hydrologique de Beauvais
     </div>
-
-    <div class="dashboard-subtitle">
-        Suivi de la hauteur d'eau, du débit, des précipitations et des prévisions
-    </div>
 </div>
 """, unsafe_allow_html=True)
 
